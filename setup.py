e1db65728cf57bc3d406e9d17d3e5846931a1a51
Ghalib92 <ghalibmohammad92@gmail.com>
2025-04-29 08:12:20 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+#set the name as local package
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Mohammed Ghalib',
+    author_email= 'ghalibmohammad92@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
